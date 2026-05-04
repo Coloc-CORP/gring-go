f0a4e2cdaf1e85517662c6f37362c4036e636a51
bolobos <cartier.dc385@gmail.com>
2026-05-04 10:51:06 +0200

    Clean addition of the NRF test file
    Version with the basic counter

---
+++ SOFTWARE/EMBEDDED/TestApplication/build/ipc_radio/modules/libmetal/libmetal/lib/compiler/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/bolobos/ncs/v3.1.1/modules/hal/libmetal/libmetal/lib/compiler
+# Build directory: /home/bolobos/Documents/gring-go/SOFTWARE/EMBEDDED/peripheral_uart/build/ipc_radio/modules/libmetal/libmetal/lib/compiler
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("armcc")
+subdirs("gcc")
+subdirs("iar")
---
+++ SOFTWARE/EMBEDDED/TestApplication/src/main.c
@@ -0,0 +1,481 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *  @brief Nordic UART Bridge Service (NUS) sample
+ */
+#include <zephyr/types.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <soc.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+
+#include <bluetooth/services/nus.h>
+
+#include <dk_buttons_and_leds.h>
+
+#include <zephyr/settings/settings.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include <zephyr/logging/log.h>
+
+#define LOG_MODULE_NAME peripheral_uart
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+
+#define STACKSIZE CONFIG_BT_NUS_THREAD_STACK_SIZE
+#define PRIORITY 7
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+#define RUN_STATUS_LED DK_LED1
+#define RUN_LED_BLINK_INTERVAL 1000
+
+#define CON_STATUS_LED DK_LED2
+
+#define KEY_PASSKEY_ACCEPT DK_BTN1_MSK
+#define KEY_PASSKEY_REJECT DK_BTN2_MSK
+
+static K_SEM_DEFINE(ble_init_ok, 0, 1);
+
+static struct bt_conn *current_conn;
+static struct bt_conn *auth_conn;
+static struct k_work adv_work;
+
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_NUS_VAL),
+};
+
+#ifdef CONFIG_UART_ASYNC_ADAPTER
+UART_ASYNC_ADAPTER_INST_DEFINE(async_adapter);
+#else
+#define async_adapter NULL
+#endif
+
+static void adv_work_handler(struct k_work *work)
+{
+	int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_2, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+
+	if (err)
+	{
+		LOG_ERR("Advertising failed to start (err %d)", err);
+		return;
+	}
+
+	LOG_INF("Advertising successfully started");
+}
+
+static void advertising_start(void)
+{
+	k_work_submit(&adv_work);
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	if (err)
+	{
+		LOG_ERR("Connection failed, err 0x%02x %s", err, bt_hci_err_to_str(err));
+		return;
+	}
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+	LOG_INF("Connected %s", addr);
+
+	current_conn = bt_conn_ref(conn);
+
+	dk_set_led_on(CON_STATUS_LED);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Disconnected: %s, reason 0x%02x %s", addr, reason, bt_hci_err_to_str(reason));
+
+	if (auth_conn)
+	{
+		bt_conn_unref(auth_conn);
+		auth_conn = NULL;
+	}
+
+	if (current_conn)
+	{
+		bt_conn_unref(current_conn);
+		current_conn = NULL;
+		dk_set_led_off(CON_STATUS_LED);
+	}
+}
+
+static void recycled_cb(void)
+{
+	LOG_INF("Connection object available from previous conn. Disconnect is complete!");
+	advertising_start();
+}
+
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+static void security_changed(struct bt_conn *conn, bt_security_t level,
+							 enum bt_security_err err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (!err)
+	{
+		LOG_INF("Security changed: %s level %u", addr, level);
+	}
+	else
+	{
+		LOG_WRN("Security failed: %s level %u err %d %s", addr, level, err,
+				bt_security_err_to_str(err));
+	}
+}
+#endif
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+	.recycled = recycled_cb,
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+	.security_changed = security_changed,
+#endif
+};
+
+#if defined(CONFIG_BT_NUS_SECURITY_ENABLED)
+static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Passkey for %s: %06u", addr, passkey);
+}
+
+static void auth_passkey_confirm(struct bt_conn *conn, unsigned int passkey)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	auth_conn = bt_conn_ref(conn);
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Passkey for %s: %06u", addr, passkey);
+
+	if (IS_ENABLED(CONFIG_SOC_SERIES_NRF54HX) || IS_ENABLED(CONFIG_SOC_SERIES_NRF54LX))
+	{
+		LOG_INF("Press Button 0 to confirm, Button 1 to reject.");
+	}
+	else
+	{
+		LOG_INF("Press Button 1 to confirm, Button 2 to reject.");
+	}
+}
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Pairing cancelled: %s", addr);
+}
+
+static void pairing_complete(struct bt_conn *conn, bool bonded)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Pairing completed: %s, bonded: %d", addr, bonded);
+}
+
+static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Pairing failed conn: %s, reason %d %s", addr, reason,
+			bt_security_err_to_str(reason));
+}
+
+static struct bt_conn_auth_cb conn_auth_callbacks = {
+	.passkey_display = auth_passkey_display,
+	.passkey_confirm = auth_passkey_confirm,
+	.cancel = auth_cancel,
+};
+
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
+	.pairing_complete = pairing_complete,
+	.pairing_failed = pairing_failed};
+#else
+static struct bt_conn_auth_cb conn_auth_callbacks;
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
+#endif
+
+static struct bt_nus_cb nus_cb = {
+	.received = NULL,
+};
+
+void error(void)
+{
+	dk_set_leds_state(DK_ALL_LEDS_MSK, DK_NO_LEDS_MSK);
+
+	while (true)
+	{
+		/* Spin for ever */
+		k_sleep(K_MSEC(1000));
+	}
+}
+
+
+
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+static void num_comp_reply(bool accept)
+{
+	if (accept)
+	{
+		bt_conn_auth_passkey_confirm(auth_conn);
+		LOG_INF("Numeric Match, conn %p", (void *)auth_conn);
+	}
+	else
+	{
+		bt_conn_auth_cancel(auth_conn);
+		LOG_INF("Numeric Reject, conn %p", (void *)auth_conn);
+	}
+
+	bt_conn_unref(auth_conn);
+	auth_conn = NULL;
+}
+
+void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	uint32_t buttons = button_state & has_changed;
+
+	if (auth_conn)
+	{
+		if (buttons & KEY_PASSKEY_ACCEPT)
+		{
+			num_comp_reply(true);
+		}
+
+		if (buttons & KEY_PASSKEY_REJECT)
+		{
+			num_comp_reply(false);
+		}
+	}
+}
+#endif /* CONFIG_BT_NUS_SECURITY_ENABLED */
+
+static void configure_gpio(void)
+{
+	int err;
+
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+	err = dk_buttons_init(button_changed);
+	if (err)
+	{
+		LOG_ERR("Cannot init buttons (err: %d)", err);
+	}
+#endif /* CONFIG_BT_NUS_SECURITY_ENABLED */
+
+	err = dk_leds_init();
+	if (err)
+	{
+		LOG_ERR("Cannot init LEDs (err: %d)", err);
+	}
+}
+
+
+int main(void)
+{
+	int blink_status = 0;
+	int err = 0;
+
+	configure_gpio();
+
+	if (IS_ENABLED(CONFIG_BT_NUS_SECURITY_ENABLED))
+	{
+		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
+		if (err)
+		{
+			LOG_ERR("Failed to register authorization callbacks. (err: %d)", err);
+			return 0;
+		}
+
+		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
+		if (err)
+		{
+			LOG_ERR("Failed to register authorization info callbacks. (err: %d)", err);
+			return 0;
+		}
+	}
+
+	err = bt_enable(NULL);
+	if (err)
+	{
+		error();
+	}
+
+	LOG_INF("Bluetooth initialized");
+
+	k_sem_give(&ble_init_ok);
+
+	if (IS_ENABLED(CONFIG_SETTINGS))
+	{
+		settings_load();
+	}
+
+	err = bt_nus_init(&nus_cb);
+	if (err)
+	{
+		LOG_ERR("Failed to initialize NUS service (err: %d)", err);
+		return 0;
+	}
+
+	LOG_INF("NUS service initialized");
+
+	k_work_init(&adv_work, adv_work_handler);
+	advertising_start();
+	
+	for (;;)
+	{
+		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+	}
+
+	return 0;
+}
+
+void ble_write_thread(void)
+{
+	/* Attendre que BLE soit initialisé */
+	k_sem_take(&ble_init_ok, K_FOREVER);
+
+	uint8_t heartrate = 60;
+	uint8_t battery = 100;
+	char nus_message[32];
+	uint16_t nus_len;
+
+	for (;;)
+	{
+		if (current_conn)
+		{
+			/* Simuler des valeurs de capteur */
+			if (heartrate < 100)
+				heartrate++;
+			else
+				heartrate = 60;
+
+			if (battery > 0)
+				battery--;
+			else
+				battery = 100;
+
+			/* Envoyer les données via NUS */
+			nus_len = snprintf(nus_message, sizeof(nus_message),
+					   "HR:%d,BAT:%d", heartrate, battery);
+
+			if (bt_nus_send(current_conn, (uint8_t *)nus_message, nus_len))
+			{
+				LOG_WRN("Failed to send NUS data");
+			}
+			else
+			{
+				LOG_INF("NUS sent: %s", nus_message);
+			}
+		}
+
+		/* Attendre 1000ms avant d'envoyer à nouveau */
+		k_sleep(K_MSEC(1000));
+	}
+}
+
+// static void bt_receive_cb(struct bt_conn *conn, const uint8_t *const data,
+// 			  uint16_t len)
+// {
+// 	int err;
+// 	char addr[BT_ADDR_LE_STR_LEN] = {0};
+
+// 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, ARRAY_SIZE(addr));
+
+// 	LOG_INF("Received data from: %s", addr);
+
+// 	for (uint16_t pos = 0; pos != len;) {
+// 		struct uart_data_t *tx = k_malloc(sizeof(*tx));
+
+// 		if (!tx) {
+// 			LOG_WRN("Not able to allocate UART send data buffer");
+// 			return;
+// 		}
+
+// 		/* Keep the last byte of TX buffer for potential LF char. */
+// 		size_t tx_data_size = sizeof(tx->data) - 1;
+
+// 		if ((len - pos) > tx_data_size) {
+// 			tx->len = tx_data_size;
+// 		} else {
+// 			tx->len = (len - pos);
+// 		}
+
+// 		memcpy(tx->data, &data[pos], tx->len);
+
+// 		pos += tx->len;
+
+// 		/* Append the LF character when the CR character triggered
+// 		 * transmission from the peer.
+// 		 */
+// 		if ((pos == len) && (data[len - 1] == '\r')) {
+// 			tx->data[tx->len] = '\n';
+// 			tx->len++;
+// 		}
+
+// 		err = uart_tx(uart, tx->data, tx->len, SYS_FOREVER_MS);
+// 		if (err) {
+// 			k_fifo_put(&fifo_uart_tx_data, tx);
+// 		}
+// 	}
+// }
+
+K_THREAD_DEFINE(ble_write_thread_id, STACKSIZE, ble_write_thread, NULL, NULL,
+				NULL, PRIORITY, 0, 0);
+
+
+
+// 				Scanner BLE
+// ├─ Nom: "Nordic nRF5340 DK"
+// └─ Clique pour se connecter
+//    ↓
+// Device Info
+// ├─ Services trouvés:
+// │  ├─ ❤️ Heart Rate Service
+// │  │   └─ BPM: 72 (mis à jour)
+// │  │
+// │  ├─ 🔋 Battery Service
+// │  │   └─ Level: 95%
+// │  │
+// │  └─ 💬 NUS (Nordic UART)
+// │      └─ Communication bi-directionnelle
+// │
+// └─ Caractéristiques avec UUID visibles
